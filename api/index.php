cde579766d61af54e2d5d99263b8d338f21b5312
rahulpvt9798 <rahul979854@gmail.com>
2026-05-16 23:13:50 +0530

    Refactor cURL headers into a separate variable

--- api/index.php
+++ api/index.php
@@ -11,17 +11,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 
 $url = "https://linearjitp-playback.astro.com.my/dash-wv/linear/";
 
-$ch = curl_init($url);
+$headers = [
+    "User-Agent: Mozilla/5.0 (Linux; Android 10; MI 9 Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/111.0.5563.58 Mobile Safari/537.36",
+    "Referer: https://astro.com.my/",
+    "Origin: https://astro.com.my"
+];
+
+$ch = curl_init();
 
 curl_setopt_array($ch, [
+    CURLOPT_URL => $url,
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_FOLLOWLOCATION => true,
     CURLOPT_SSL_VERIFYPEER => false,
-    CURLOPT_HTTPHEADER => [
-        "User-Agent: Mozilla/5.0 (Linux; Android 10; MI 9 Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/111.0.5563.58 Mobile Safari/537.36",
-        "Referer: https://astro.com.my/",
-        "Origin: https://astro.com.my"
-    ]
+    CURLOPT_HTTPHEADER => $headers
 ]);
 
 $response = curl_exec($ch);
